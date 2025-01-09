fd415a025d0328496b8d5fe2458fbba7fcda41fd
Maasid <maasidbhat@gmail.com>
2025-01-09 16:10:31 +0530

    external js

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
-console.log(1);
-console.log(4);
-console.log(3);
-console.log(2);
+let cartQuantity = 0;
+
+const showButton = document.getElementById("show");
+const addToCartButton = document.getElementById("add-to-cart");
+const addtwiceButton = document.getElementById("add-twice");
+const resetButton = document.getElementById("reset");
+const removeButton = document.getElementById("remove-button");
+
+function showQuantity() {
+  console.log(`The quantity is ${cartQuantity}`);
+}
+function addToCart(toAdd) {
+  cartQuantity = cartQuantity + toAdd;
+  console.log(`New number of items ${cartQuantity}`);
+}
+
+function reset() {
+  cartQuantity = 0;
+  console.log("The Cart was reset");
+  console.log(`New number of items ${cartQuantity}`);
+}
+function remove() {
+  cartQuantity--;
+  console.log(`New number of items ${cartQuantity}`);
+}
+
+showButton.addEventListener("click", showQuantity);
+addToCartButton.addEventListener("click", function () {
+  addToCart(1);
+});
+addtwiceButton.addEventListener("click", () => addToCart(2));
+resetButton.addEventListener("click", reset);
+removeButton.addEventListener("click", remove);
