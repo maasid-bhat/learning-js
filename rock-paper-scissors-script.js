f10a4ad3aba375fe61d3a3b73a5261c64c3bea87
Maasid <maasidbhat@gmail.com>
2025-01-13 13:42:25 +0530

    learned objects and made a score board for the rock papers and scissors project

--- rock-paper-scissors-script.js
+++ rock-paper-scissors-script.js
@@ -1,6 +1,12 @@
+
+const score ={
+  wins :0,
+  losses :0,
+  ties :0
+}
 function utilityFunctionForComputerMove() {
   const randomNumber = Math.random();
-
+  
   let computerMove = "";
 
   if (randomNumber >= 0 && randomNumber < 1 / 4) {
@@ -21,28 +27,44 @@ let result = "";
 // Rock button
 
 const rockButton = document.getElementById("rock");
+
 rockButton.addEventListener("click", function () {
   const computerMove = utilityFunctionForComputerMove();
-
+  const playerMove ="rock"
+if (playerMove === "rock"){
   if (computerMove === "rock") {
     result = "Tie!";
   } else if (computerMove === "paper") {
-    result = "You lose!";
+    result = "You Loose!";
   } else if (computerMove === "scissors") {
     result = "You win!";
   } else if (computerMove === "Thermonuclear Warhead") {
-    result = "You lose, lmao";
-  }
+    result = "You Loose, lmao";
+  }}
+   if(result==="You win!"){
+    score.wins += 1;
+   }else if(result==="You Loose!"){
+    score.losses += 1;
+   }else if(result==="You Loose, lmao"){
+    score.losses += 1;
+   }else if(result=== "Tie!"){
+    score.ties += 1;
+   }
+  
+  alert(`You picked ${playerMove}, computer picked ${computerMove},${result}
+Wins:${score.wins},Losses:${score.losses},Ties:${score.ties}`)});
 
-  alert(`You picked rock, computer picked ${computerMove},${result}`);
-});
 
 // Paper button
 
 const paperButton = document.getElementById("paper");
+
 paperButton.addEventListener("click", function () {
   const computerMove = utilityFunctionForComputerMove();
 
+  playerMove="paper";
+  
+ if (playerMove === "paper"){
   if (computerMove === "rock") {
     result = "You Win!";
   } else if (computerMove === "paper") {
@@ -50,18 +72,33 @@ paperButton.addEventListener("click", function () {
   } else if (computerMove === "scissors") {
     result = "You Loose!";
   } else if (computerMove === "Thermonuclear Warhead") {
-    result = "You lose,lmao";
+    result = "You Loose, lmao";
   }
+  if(result==="You Win!"){
+    score.wins += 1;
+   }else if(result==="You Loose!"){
+    score.losses += 1;
+   }else if(result==="You Loose, lmao"){
+    score.losses += 1;
+   }else if(result=== "Tie!"){
+    score.ties += 1;
+   }
+  
 
-  alert(`You picked paper, computer picked ${computerMove},${result}`);
+  alert(`You picked ${playerMove}, computer picked ${computerMove},${result}
+  Wins:${score.wins},Losses:${score.losses},Ties:${score.ties}`);}
 });
 
 // Scissors button
 
 const scissorsButton = document.getElementById("scissors");
+
 scissorsButton.addEventListener("click", function () {
   const computerMove = utilityFunctionForComputerMove();
 
+  playerMove="scissors";
+
+ if(playerMove==="scissors"){
   if (computerMove ==='rock'){
     result='You Loose!';
   }else if(computerMove ==='paper'){
@@ -69,7 +106,31 @@ scissorsButton.addEventListener("click", function () {
   }else if(computerMove === 'scissors'){
     result= 'Tie!';
   }else if (computerMove === 'Thermonuclear Warhead'){
-    result= 'You lose,lmao';
-  }
-  alert(`You picked Scissors, computer picked ${computerMove},${result}`)
+    result= 'You Loose,lmao';
+  }if(result==="You win!"){
+    score.wins += 1;
+   }else if(result==="You Loose!"){
+    score.losses += 1;
+   }else if(result==="You Loose, lmao"){
+    score.losses += 1;
+   }else if(result=== "Tie!"){
+    score.ties += 1;
+   }
+  
+  alert(`You picked ${playerMove}, computer picked ${computerMove},${result}
+  Wins:${score.wins},Losses:${score.losses},Ties:${score.ties}`)}
 });
+//reset button
+function resetfunc(){
+  score.wins=0;
+  score.losses=0;
+  score.ties=0;
+}
+
+const resetButton = document.getElementById("reset");
+resetButton.addEventListener("click",function(){const resetButton= resetfunc();
+playerMove="Reset button"
+if(playerMove==="Reset button"){
+  resetfunc()
+  alert("Score has been reset");
+}})
