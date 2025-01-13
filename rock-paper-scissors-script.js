a42eafb9dcf16144aa07340cb6a88074b7471a64
Maasid <maasidbhat@gmail.com>
2025-01-13 16:06:17 +0530

    Use functions for reusablity

--- rock-paper-scissors-script.js
+++ rock-paper-scissors-script.js
@@ -1,22 +1,36 @@
+let score = {
+  wins: 0,
+  losses: 0,
+  ties: 0,
+};
 
-const score ={
-  wins :0,
-  losses :0,
-  ties :0
-}
-function utilityFunctionForComputerMove() {
+const moveList = {
+  ROCK: "rock",
+  PAPER: "paper",
+  SCISSORS: "scissors",
+  THERMO_NUCLEAR_WARHEAD: "Thermo-nuclear Warhead",
+};
+
+const resultsList = {
+  WIN: "You Win!",
+  LOOSE: "You Loose!",
+  VIOLATION: "You Loose, lmao!",
+  TIE: "Tie!",
+};
+
+function getComputerMove() {
   const randomNumber = Math.random();
-  
+
   let computerMove = "";
 
   if (randomNumber >= 0 && randomNumber < 1 / 4) {
-    computerMove = "rock";
+    computerMove = moveList.ROCK;
   } else if (randomNumber >= 1 / 4 && randomNumber < 2 / 4) {
-    computerMove = "paper";
+    computerMove = moveList.PAPER;
   } else if (randomNumber >= 2 / 4 && randomNumber < 3 / 4) {
-    computerMove = "scissors";
-  } else if (randomNumber >= 3 / 4 && randomNumber <= 1) {
-    computerMove = "Thermonuclear Warhead";
+    computerMove = moveList.SCISSORS;
+  } else {
+    computerMove = moveList.THERMO_NUCLEAR_WARHEAD;
   }
 
   return computerMove;
@@ -24,113 +38,92 @@ function utilityFunctionForComputerMove() {
 
 let result = "";
 
-// Rock button
-
-const rockButton = document.getElementById("rock");
-
-rockButton.addEventListener("click", function () {
-  const computerMove = utilityFunctionForComputerMove();
-  const playerMove ="rock"
-if (playerMove === "rock"){
-  if (computerMove === "rock") {
-    result = "Tie!";
-  } else if (computerMove === "paper") {
-    result = "You Loose!";
-  } else if (computerMove === "scissors") {
-    result = "You win!";
-  } else if (computerMove === "Thermonuclear Warhead") {
-    result = "You Loose, lmao";
-  }}
-   if(result==="You win!"){
+function trackScore() {
+  if (result === resultsList.WIN) {
     score.wins += 1;
-   }else if(result==="You Loose!"){
-    score.losses += 1;
-   }else if(result==="You Loose, lmao"){
-    score.losses += 1;
-   }else if(result=== "Tie!"){
+  } else if (result === resultsList.TIE) {
     score.ties += 1;
-   }
-  
-  alert(`You picked ${playerMove}, computer picked ${computerMove},${result}
-Wins:${score.wins},Losses:${score.losses},Ties:${score.ties}`)});
+  } else {
+    score.losses += 1;
+  }
+}
 
+function getResult(playerMove) {
+  const computerMove = getComputerMove();
+
+  if (playerMove === moveList.ROCK) {
+    if (computerMove === moveList.ROCK) {
+      result = resultsList.TIE;
+    } else if (computerMove === moveList.PAPER) {
+      result = resultsList.LOOSE;
+    } else if (computerMove === moveList.SCISSORS) {
+      result = resultsList.WIN;
+    } else {
+      result = resultsList.VIOLATION;
+    }
+  } else if (playerMove === moveList.PAPER) {
+    if (computerMove === moveList.ROCK) {
+      result = resultsList.WIN;
+    } else if (computerMove === moveList.PAPER) {
+      result = resultsList.TIE;
+    } else if (computerMove === moveList.SCISSORS) {
+      result = resultsList.LOOSE;
+    } else {
+      result = resultsList.VIOLATION;
+    }
+  } else {
+    if (computerMove === moveList.ROCK) {
+      result = resultsList.LOOSE;
+    } else if (computerMove === moveList.PAPER) {
+      result = resultsList.WIN;
+    } else if (computerMove === moveList.SCISSORS) {
+      result = resultsList.TIE;
+    } else {
+      result = resultsList.VIOLATION;
+    }
+  }
+
+  trackScore();
+
+  return computerMove;
+}
+
+function getAlert(playerMove, computerMove) {
+  alert(`You picked ${playerMove}, computer picked ${computerMove},${result}
+  Wins:${score.wins},Losses:${score.losses},Ties:${score.ties}`);
+}
+
+// Rock button
+const rockButton = document.getElementById(moveList.ROCK);
+rockButton.addEventListener("click", function () {
+  const playerMove = moveList.ROCK;
+  const computerMove = getResult(playerMove);
+  getAlert(playerMove, computerMove);
+});
 
 // Paper button
-
-const paperButton = document.getElementById("paper");
-
+const paperButton = document.getElementById(moveList.PAPER);
 paperButton.addEventListener("click", function () {
-  const computerMove = utilityFunctionForComputerMove();
-
-  playerMove="paper";
-  
- if (playerMove === "paper"){
-  if (computerMove === "rock") {
-    result = "You Win!";
-  } else if (computerMove === "paper") {
-    result = "Tie!";
-  } else if (computerMove === "scissors") {
-    result = "You Loose!";
-  } else if (computerMove === "Thermonuclear Warhead") {
-    result = "You Loose, lmao";
-  }
-  if(result==="You Win!"){
-    score.wins += 1;
-   }else if(result==="You Loose!"){
-    score.losses += 1;
-   }else if(result==="You Loose, lmao"){
-    score.losses += 1;
-   }else if(result=== "Tie!"){
-    score.ties += 1;
-   }
-  
-
-  alert(`You picked ${playerMove}, computer picked ${computerMove},${result}
-  Wins:${score.wins},Losses:${score.losses},Ties:${score.ties}`);}
+  const playerMove = moveList.PAPER;
+  const computerMove = getResult(playerMove);
+  getAlert(playerMove, computerMove);
 });
 
 // Scissors button
-
-const scissorsButton = document.getElementById("scissors");
-
+const scissorsButton = document.getElementById(moveList.SCISSORS);
 scissorsButton.addEventListener("click", function () {
-  const computerMove = utilityFunctionForComputerMove();
-
-  playerMove="scissors";
-
- if(playerMove==="scissors"){
-  if (computerMove ==='rock'){
-    result='You Loose!';
-  }else if(computerMove ==='paper'){
-    result= 'You Win!';
-  }else if(computerMove === 'scissors'){
-    result= 'Tie!';
-  }else if (computerMove === 'Thermonuclear Warhead'){
-    result= 'You Loose,lmao';
-  }if(result==="You win!"){
-    score.wins += 1;
-   }else if(result==="You Loose!"){
-    score.losses += 1;
-   }else if(result==="You Loose, lmao"){
-    score.losses += 1;
-   }else if(result=== "Tie!"){
-    score.ties += 1;
-   }
-  
-  alert(`You picked ${playerMove}, computer picked ${computerMove},${result}
-  Wins:${score.wins},Losses:${score.losses},Ties:${score.ties}`)}
+  const playerMove = moveList.SCISSORS;
+  const computerMove = getResult(playerMove);
+  getAlert(playerMove, computerMove);
 });
-//reset button
-function resetfunc(){
-  score.wins=0;
-  score.losses=0;
-  score.ties=0;
-}
 
+// Reset button
 const resetButton = document.getElementById("reset");
-resetButton.addEventListener("click",function(){const resetButton= resetfunc();
-playerMove="Reset button"
-if(playerMove==="Reset button"){
-  resetfunc()
+resetButton.addEventListener("click", function () {
+  score = {
+    wins: 0,
+    losses: 0,
+    ties: 0,
+  };
   alert("Score has been reset");
-}})
+});
